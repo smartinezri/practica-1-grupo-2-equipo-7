b647f74cf763bd98d96c517814e3b25dda5acda4
AlejaMuGo <mamunozgo@unal.edu.co>
2023-10-09 13:13:45 -0500

    Creación GestionUsuario

--- src/Aerolinea/Usuario.java
+++ src/Aerolinea/Usuario.java
@@ -11,8 +11,17 @@ public class Usuario {
 	private String nombre;
 	private int millas;
 	private ArrayList<Boleto> historial = new ArrayList<Boleto>();
+	private String correo_electronico;
+	private String contrasena;
 
 	public Usuario() {
+	}
+
+	public Usuario(String nombre, String id, String correo_electronico, String contrasena){
+		this.nombre = nombre;
+		this.id = id;
+		this.correo_electronico= correo_electronico;
+		this.contrasena = contrasena;
 	}
 
 	public void comprarBoleto(Boleto boleto) {
@@ -72,6 +81,20 @@ public class Usuario {
 	public void setHistorial(ArrayList<Boleto> historial) {
 		this.historial = historial;
 	}
+	public String getCorreo_electronico() {
+		return this.correo_electronico;
+	}
 
+	public void setCorreo_electronico(String correo_electronico) {
+		this.correo_electronico = correo_electronico;
+	}
+
+	public String getContrasena() {
+		return this.contrasena;
+	}
+
+	public void setContrasena(String contrasena) {
+		this.contrasena = contrasena;
+	}
 
 }
